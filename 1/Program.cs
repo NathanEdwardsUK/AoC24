6d399f3f5488bca641577f8e2d9d8623f5461504
Nathan Edwards <nathanedwardsuk@gmail.com>
2024-12-06 13:16:54 +0100

    First commit. Pushing my solution to day1 of AoC 2024

---
+++ 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+string line = "";
+List<int> list1 = []; 
+List<int> list2 = []; 
+List<int> listDiff = []; 
+
+StreamReader sr = new StreamReader("/Users/nathanedwards/Dev/AoC/1/input.txt");
+
+line = sr.ReadLine();
+while (line != null)
+{
+	string[] splitString = line.Split(" ");
+	List<int> intList = [];
+
+	foreach (var s in splitString)
+	{
+		int x = 0;
+		bool isInt = Int32.TryParse(s, out x);
+		
+		if (isInt)
+		{
+			intList.Add(x);
+		}
+	}
+	list1.Add(intList[0]);
+	list2.Add(intList[1]);
+
+	line = sr.ReadLine();
+}
+
+list1.Sort();
+list2.Sort();
+
+for (int i = 0; i <list1.Count; i++)
+{
+	int diff = Math.Abs(list1[i] - list2[i]);
+	listDiff.Add(diff); 
+	Console.WriteLine($"{i} - {list1[i]};{list2[i]} - {listDiff[i]}");
+}
+
+int sum = listDiff.Sum();
+Console.WriteLine(sum);
+
