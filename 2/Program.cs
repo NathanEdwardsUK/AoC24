7e081a5738e32c51270d8695c93c2d2696b3c292
Nathan Edwards <nathanedwardsuk@gmail.com>
2024-12-09 15:59:03 +0100

    reversed erroneous overwrite of day 2 solutions

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,26 +1,5 @@
 ﻿using System.IO;
-
-StreamReader sr = new StreamReader("/Users/nathanedwards/Dev/AoC/3/input.txt");
-linr = sr.ReadLine();
-Console.Write(line);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* string line = "";
+string line = "";
 List<List<int>> inputList = []; 
 List<bool> safeList = []; 
 
@@ -124,4 +103,4 @@ while ((line = sr.ReadLine()) != null)
 }
 
 Console.Write($"Solution part 1 = {safeCount1}\n");
-Console.Write($"Solution part 2 = {safeCount2}\n"); */
+Console.Write($"Solution part 2 = {safeCount2}\n");
